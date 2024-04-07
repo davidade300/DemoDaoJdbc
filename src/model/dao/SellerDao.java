f9016aa162961e9e22bdd7d0a1e39c033f12a10b
davidade300 <iaderinho@outlook.com>
2024-04-06 23:38:14 -0300

    findByDepartment Implemented

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,18 +1,15 @@
 package model.dao;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.List;
 
 public interface SellerDao {
-
     void insert(Seller obj); //insere no bd o objeto enviado como parametro de entrada
-
     void update(Seller obj);
-
     void deleteById(Integer id);
-
     Seller findById(Integer id); //consulta no bd, a partir do id, se existir retorna, se nao, retorna nulo
-
     List<Seller> findAll();
+    List<Seller> findByDepartment(Department department);
 }
