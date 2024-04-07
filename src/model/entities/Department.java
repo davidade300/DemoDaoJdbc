52ca6a64a8dd259bf5f63c1d9ed18a1c01f8f118
davidade300 <iaderinho@outlook.com>
2024-04-06 21:31:17 -0300

    Department entity class created

---
+++ src/model/entities/Department.java
@@ -0,0 +1,60 @@
+package model.entities;
+
+import java.io.Serializable;
+
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Department that = (Department) o;
+        return id == that.id && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [id: " + id + ", name: " + name + "]";
+    }
+
+}
+
