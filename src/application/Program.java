7aa5cb9829cc699afed9ae1ab6160726503ad066
davidade300 <iaderinho@outlook.com>
2024-04-07 19:14:59 -0300

    update seller implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,12 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
 
+        System.out.println("\n=== TEST 5: seller seller insert ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed! ");
+
+
     }
 }
